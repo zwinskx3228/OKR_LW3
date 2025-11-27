bc429eeca28d74aaad344555a6afefe325de866d
Uliana <ulfweey@gmail.com>
2025-11-27 19:44:11 +0200

    form1 was designed

---
+++ GuessGame/App.WinForms/Menu.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace App.WinForms
+{
+    partial class Menu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(96, 130, 182);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button1.Location = new Point(92, 127);
+            button1.Name = "button1";
+            button1.Size = new Size(253, 55);
+            button1.TabIndex = 0;
+            button1.Text = "Start the game";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(96, 130, 182);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button2.Location = new Point(92, 220);
+            button2.Name = "button2";
+            button2.Size = new Size(253, 55);
+            button2.TabIndex = 1;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Castellar", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(409, 36);
+            label1.TabIndex = 2;
+            label1.Text = "Game Guess a number";
+            label1.Click += label1_Click;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(167, 199, 231);
+            ClientSize = new Size(441, 336);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Menu";
+            Text = "Menu";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+    }
+}
---
+++ GuessGame/App.WinForms/Menu.cs
@@ -0,0 +1,15 @@
+namespace App.WinForms
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
